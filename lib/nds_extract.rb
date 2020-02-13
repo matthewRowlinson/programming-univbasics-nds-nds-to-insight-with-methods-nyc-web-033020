134760cfc9625b26dcd2e780ddf39d4aed082552
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-13 15:41:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,6 +25,18 @@ end
 
 
 def directors_totals(nds)
+  this_director_gross = 0 
   result = {}
-  nil
+  director_i = 0 
+  while director_i < nds.count do 
+    movie_i = 0 
+    while movie_i < nds[director_i][:movies].count do 
+    this_director_gross += nds[director_i][:movies][movie_i][:worldwide_gross]
+    movie_i += 1 
+    end
+    result[nds[director_i][:name]] = this_director_gross
+    this_director_gross = 0 
+    director_i += 1 
+  end
+  result
 end
